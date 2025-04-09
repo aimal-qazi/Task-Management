eabc24b2888922aa291d3f06717cbac81bb1001b
aimal-qazi <aimalqazi32@gmail.com>
2025-04-09 14:22:32 +0500

    install react icon library and remove commented code

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import './../styles/auth.css'
+import { IoIosLock } from 'react-icons/io'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="auth-container">
+      <div className="auth-wrapper">
+        <div className="auth-header">
+          <div className="icon">
+            <IoIosLock />
+          </div>
+          <h2>Welcome back</h2>
+          <p>Sign in to your account to continue</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
