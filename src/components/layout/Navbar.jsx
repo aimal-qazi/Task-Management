fc16cbcfc4d91176679dd674931913f634ee399e
aimal-qazi <aimalqazi32@gmail.com>
2025-04-10 14:04:33 +0500

    improve UI of login, register, navbar and home component

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import './../../styles/navbar.css'
+import { FaUserCircle } from 'react-icons/fa'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar-container">
+      <h1>Task Management</h1>
+
+      <div className="user">
+        <FaUserCircle className='icon'/>
+        <div className='user-info'>
+          <h4>John Doe</h4>
+          <p>john@gmail.com</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
